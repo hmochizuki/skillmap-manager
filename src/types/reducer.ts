bad61f57ca0b213bc4ca178835839099cda91366
h_mochizuki <bda5114128@gmail.com>
2020-08-28 00:52:37 +0900

    feat: implements actionCreatorFactory

--- src/types/reducer.ts
+++
@@ -1,6 +0,0 @@
-export type Action<T extends string, P = undefined, M = undefined> = {
-  type: T;
-  payload: P;
-  meta?: M;
-  error?: boolean;
-};
--- src/util/reducers.ts
+++ src/util/reducers.ts
@@ -1,12 +1,56 @@
-import { Action } from "types/reducer";
+interface AnyAction {
+  type: any;
+}
 
-export const createAction = <T extends string, P, M>(type: T) => (
-  payload: P,
-  meta?: M,
-  error?: boolean
-): Action<T, P, M> => ({
-  type,
-  payload,
-  meta,
-  error,
-});
+type Meta = null | { [key: string]: any };
+
+export interface Action<Payload> extends AnyAction {
+  type: string;
+  payload: Payload;
+  error?: boolean;
+  meta?: Meta;
+}
+
+interface ActionCreator<Payload> {
+  type: string;
+  (payload: Payload, meta?: Meta): Action<Payload>;
+}
+
+export const actionCreatorFactory = (
+  prefix: string,
+  defaultIsError: (payload: any) => boolean = (p) => p instanceof Error
+) => {
+  const base = `${prefix}/`;
+
+  const actionCreator = <Payload>(
+    type: string,
+    commonMeta?: Meta,
+    isError: ((payload: Payload) => boolean) | boolean = defaultIsError
+  ) => {
+    const fullType = base + type;
+
+    return Object.assign(
+      (payload: Payload, meta?: Meta) => {
+        const action: Action<Payload> = {
+          type: fullType,
+          payload,
+        };
+
+        if (commonMeta || meta) {
+          action.meta = { ...commonMeta, ...meta };
+        }
+
+        if (isError && (typeof isError === "boolean" || isError(payload))) {
+          action.error = true;
+        }
+
+        return action;
+      },
+      {
+        type: fullType,
+      }
+    ) as ActionCreator<Payload>;
+  };
+
+  return actionCreator;
+};
